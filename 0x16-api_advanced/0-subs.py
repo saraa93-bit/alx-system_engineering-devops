b671b3a44e5eb2fe30aef41f2837c32e64811cf1
saraa93-bit <saraa93177228@gmail.com>
2024-06-06 23:16:34 +0300

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'my-reddit-subscriber-counter/0.1'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, timeout=10, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = data['data']['subscribers']
+    else:
+        count = 0
+    return count
