6044bc61f4af4fe996313ff3eaf9ae5c8cf33fb8
saraa93-bit <saraa93177228@gmail.com>
2024-06-06 23:37:29 +0300

    api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+
 """
 function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-"""
+prints the titles of the first 10 hot posts"""
 
 import requests
 
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'my-reddit-post-fetcher/0.1'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
 
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, timeout=10,
+                            params={"limit": 9}, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = data['data']['children']
+        for i in range(0, 10):
+            print(count[i]['data']['title'])
+    else:
+        print('None')
